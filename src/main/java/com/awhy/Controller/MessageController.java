67b86a7682c86d9a040eaaac9df223d6223fbd01
carvin09 <enzo.caruso@gmail.com>
2014-01-18 19:00:17 +0100

    Create Business Logic and define all classes

---
+++ src/main/java/com/awhy/Controller/MessageController.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.awhy.Controller;
+
+/**
+ *
+ * @author Enzo
+ */
+public class MessageController {
+    
+}
---
+++ src/main/java/com/awhy/Service/MessageService.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.awhy.Service;
+
+import com.awhy.Model.AnswerMessage;
+
+/**
+ *
+ * @author Enzo
+ */
+public class MessageService {
+    
+    //query from MongoDB
+    public AnswerMessage getAnswer(){
+        
+        AnswerMessage answer_message = null;
+    
+        return answer_message;
+    }
+    
+    
+    
+    
+}
